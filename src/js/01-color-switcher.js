fc7052ca6301b5be728a726e2cd7e5593ee4e337
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-08-03 19:29:51 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let changeColorId = null;
 
+startBtn.addEventListener("click", changeColorStart);
+stopBtn.addEventListener("click", stopColorChange);
+
+
+function changeColorStart() {
+  changeColorId = setInterval(() => {
+    document.body.style.background = getRandomHexColor()
+  }, 1000);
+  startBtn.setAttribute("disabled", true);
+  stopBtn.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(changeColorId);
+  stopBtn.setAttribute("disabled", true);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
